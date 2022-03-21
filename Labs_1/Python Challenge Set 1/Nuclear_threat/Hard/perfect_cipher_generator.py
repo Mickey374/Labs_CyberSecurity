36e590b59390c89fa71d448c2651620acd59e688
LijTesfaye <mesay1964@gmail.com>
2022-03-21 15:07:17 +0100

    add files

--- Labs_1/Python Challenge Set 1/Nuclear_threat/Hard/perfect_cipher_generator.py
+++ Labs_1/Python Challenge Set 1/Nuclear_threat/Hard/perfect_cipher_generator.py
@@ -8,23 +8,26 @@ random.seed(time.time())
 #GOOD LUCK BRUTEFORCING THEM! AHAHAHAHAHAHAHAHAHAHAHAHAHAH
 A=random.randrange(2**128)
 C=random.randrange(2**128)
+#from here
 n=256
 k0=int(input("Insert shared-secret (k0): "))
 k0=k0%n
+#to here
 #Insert the message
 pt = input("Message to be encrypted: \n")
 if pt[0:6]!="From: ":
 	print("For security reasons anonymous messages are not allowed!")
 	exit()
-pt=pt.encode('ASCII')
+pt=pt.encode('ASCII') # strings are changes to their ASCII equivalent
 ct=[]
 ki=k0
 #Encrypt the message
-print(A)
-print(C)
+print('A is : ',A)
+print('C is : ',C)
 for i in range(len(pt)):
 	ct.append(ki ^ pt[i])
 	ki = (A*ki + C)%n
 #Write to file
 with open("ciphertext.txt", "wb") as f:
     f.write(bytes(ct))
+f.close()
